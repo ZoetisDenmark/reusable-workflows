207d4ab3ed0cc408a4e7afb82662967d147d0e23
Nicolas Pépin-Hermann <Nicolas.PepinHermann@zoetis.com>
2023-05-06 16:07:28 +0200

    Add logging library to Python demo script.

--- variable-feeder.py
+++ variable-feeder.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 print("VF1=I AM VF1")
 print("VF2=I AM VF2")
 
-os.write(2, b'VF_ERR=I AM VF_ERR\n')
-os.write(2, b'I am just a simple error message.\n')
+logging.info('VF_ERR=I AM VF_ERR')
+logging.error('I am just a simple error message.')
